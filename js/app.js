72c29be21dbe650462e923cd778840b0427d897e
shi_chikuzen <69347287+shi-chikuzen@users.noreply.github.com>
2022-01-20 20:17:07 +0900

    add valid createSchool

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 // Copyright (c) 2022 @shi_chikuzen
 
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#00aeb9',
+                success: '#00aeb9',
+            },
+        },
+    },
+});
+
 var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
         fileSelected: null,
         workbook: null,
-        state: { "fileError": false, "ready": false, "loading": false },
+        state: { "fileError": false, "ready": false, "loading": false, "message": [], "errorSnack": false },
         default: {
             "color": ["#47cacc", "#63bcc9", "#cdb3d4", "#e7b7c8", "#ffbe88"],
             "gradient": [
@@ -16,62 +29,121 @@ var app = new Vue({
                 "linear-gradient(127deg, #e7b7c8, #cdb3d4)",
                 "linear-gradient(127deg, #ffbe88, #e7b7c8)",
             ],
+            "sheetNames": { "category": "カテゴリー", "character": "キャラクター", "school": "教育課程", "event": "イベント" },
+            "colNames": {
+                "category": { "name": "カテゴリ名", "color": "カテゴリ色", "bgcolor": "カテゴリ色" },
+                "character": { "name": "キャラクタ名", "category": "カテゴリ", "birthday": "誕生日（yyyy/MM/dd）", "autoBirth": "誕生年自動計算", "schoolName": "教育課程名", "schoolPeriod": "教育課程年数" },
+                "school": { "name": "名称", "period": "年数", "month": "開始月", "age": "開始年齢" },
+                "event": { "category": "カテゴリ", "title": "タイトル", "year": "年", "month": "月", "day": "日", "hour": "月", "minute": "分", "detail": "詳細" },
+            },
         },
-        data: { "settings": {"category": {}, "character": {}, "school": {},}, "event": []},
+        data: { "settings": { "category": {}, "character": {}, "school": {}, }, "event": [] },
     },
     mounted: function () { },
     watch: {
         workbook: 'init'
     },
     methods: {
+        getObjectKey(obj, val) { // dictをvalで逆引きする
+            const key = Object.keys(obj).filter(key => {
+                return obj[key] === val;
+            })
+            return key;
+        },
+        convertSerial2Date(serial) { // エクセルのシリアル値をDateに変換する
+            const coef = 24 * 60 * 60 * 1000;
+            const offset = 70 * 365 + 17 + 1 + 1;
+            const diff = 9 * 60 * 60 * 1000;
+            const unixtime = (serial - offset) * coef - diff;
+            return new Date(unixtime);
+        },
         readFile: function (file) { // xlsx読み込み
             this.state.loading = true;
             const vm = this;
             const reader = new FileReader();
             reader.onload = function (e) {
                 let data = e.target.result;
-                vm.workbook = XLSX.read(data, {'type': 'binary'})
+                vm.workbook = XLSX.read(data, { 'type': 'binary' })
             };
             reader.readAsBinaryString(file);
         },
-        validData() { // 指定シートがファイルに存在するかを返す
-            const sheetNames = ["カテゴリー", "キャラクター", "教育課程", "イベント"]
+        validData() { // 必要なシート・列がファイルに存在するかを返す
+            this.state.message = [];
+            const sheetNames = Object.values(this.default.sheetNames);
             let valid = true;
-            for (let i = 0; i < sheetNames.length; i++){
+            for (let i = 0; i < sheetNames.length; i++) {
                 if (this.workbook.SheetNames.indexOf(sheetNames[i]) == -1) {
+                    this.state.message.push(`以下のシートが存在しません: ${sheetNames[i]}`);
                     valid = false;
+                } else {
+                    const data = XLSX.utils.sheet_to_json(this.workbook.Sheets[sheetNames[i]], { header: 1 });
+                    const key = this.getObjectKey(this.default.sheetNames, sheetNames[i]);
+                    const colNames = Object.values(this.default.colNames[key]);
+                    for (let j = 0; j < colNames.length; j++) {
+                        if (data[0].indexOf(colNames[j]) == -1) {
+                            this.state.message.push(`${sheetNames[i]}シートに以下の列が存在しません: ${colNames[j]}`);
+                            valid = false;
+                        };
+                    };
                 };
             };
             this.state.fileError = !valid;
             return valid;
         },
+        getBirth(data) { // 誕生年を計算してDate形式で返す
+            return "true";
+        },
         createCategory() { // カテゴリ設定を読み込んでフォーマット
-            const data = XLSX.utils.sheet_to_json(this.workbook.Sheets["カテゴリー"], { header: 0 });
+            const data = XLSX.utils.sheet_to_json(this.workbook.Sheets[this.default.sheetNames["category"]], { header: 0 });
+            const colNames = this.default.colNames["category"];
             for (let i = 0; i < data.length; i++) { // 各データを処理
                 let row = {
-                    "color": ("カテゴリ色" in data[i]) ? data[i]["カテゴリ色"] : this.default.color[i % this.default.color.length],
-                    "bgcolor": ("カテゴリ色" in data[i]) ? data[i]["カテゴリ色"] : this.default.gradient[i % this.default.color.length],
+                    "color": (colNames["color"] in data[i]) ? data[i]["カテゴリ色"] : this.default.color[i % this.default.color.length],
+                    "bgcolor": (colNames["color"] in data[i]) ? data[i]["カテゴリ色"] : this.default.gradient[i % this.default.color.length],
+                    "characters": [],
                 };
-                this.data.settings.category[data[i]["カテゴリ名"]] = row;
+                this.data.settings.category[data[i][colNames["name"]]] = row;
             };
         },
         createSchool() { // 教育課程設定を読み込んでフォーマット
-            const data = XLSX.utils.sheet_to_json(this.workbook.Sheets["教育課程"], { header: 0 });
+            const data = XLSX.utils.sheet_to_json(this.workbook.Sheets[this.default.sheetNames["school"]], { header: 0 });
+            const colNames = this.default.colNames["school"];
             for (let i = 0; i < data.length; i++) { // 各データを処理
                 let row = {
-                    "period": Number(data[i]["年数"]),
-                    "month": Number(data[i]["開始月"]),
-                    "age": Number(data[i]["開始年齢"]),
+                    "period": Number(data[i][colNames["period"]]),
+                    "month": Number(data[i][colNames["month"]]),
+                    "age": Number(data[i][colNames["age"]]),
                 };
-                this.data.settings.school[data[i]["名称"]] = row;
+                this.data.settings.school[data[i][colNames["name"]]] = row;
             };
         },
-        formatData() { // 読み込んだxlsxをフォーマット
+        createCharacter() {
+            const data = XLSX.utils.sheet_to_json(this.workbook.Sheets[this.default.sheetNames["character"]], { header: 0 });
+            const colNames = this.default.colNames["character"];
+            for (let i = 0; i < data.length; i++) { // 各データを処理
+                const row = data[i];
+                if (!(row[colNames["category"]] in this.data.settings.category)) { // 指定カテゴリが存在しない
+                    this.state.message.push(`キャラクター「${row[colNames["name"]]}」に指定されたカテゴリ「${row[colNames["category"]]}」の設定が存在しません`);
+                    continue;
+                };
+                let result = {
+                    "category": row[colNames["category"]],
+                    "birthday": !(row[colNames["autoBirth"]]) ? this.convertSerial2Date(row[colNames["birthday"]]) : this.getBirth(row),
+                };
+                this.data.settings.character[row[colNames["name"]]] = result;
+            };
+        },
+        async formatData() { // 読み込んだxlsxをフォーマット
+            this.state.message = [];
             if (this.validData()) {
-                this.createCategory();
-                this.createSchool();
+                await this.createCategory();
+                await this.createSchool();
+                await this.createCharacter();
                 console.info(this.data);
-            }
+            };
+            if (this.state.message.length != 0) {
+                this.state.errorSnack = true;
+            };
         },
         async init() { // 初期化処理
             this.state.ready = false;
@@ -80,4 +152,4 @@ var app = new Vue({
             this.state.ready = true;
         }
     },
-})
+});
